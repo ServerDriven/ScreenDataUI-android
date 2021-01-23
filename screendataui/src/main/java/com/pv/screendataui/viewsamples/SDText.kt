b7d17883ef5794a9f3aa7bb22fb7b9d9cf724cc9
pv <parshav@live.com>
2021-01-23 17:13:11 -0600

    Further final SDText

--- screendataui/src/main/java/com/pv/screendataui/viewsamples/SDText.kt
+++ screendataui/src/main/java/com/pv/screendataui/viewsamples/SDText.kt
@@ -5,13 +5,17 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.pv.screendata.objects.SomeColor
 import com.pv.screendata.objects.SomeStyle
 import com.pv.screendata.types.Alignment
 import com.pv.screendata.views.SomeText
 import com.pv.screendataui.SDScreenDemo.hexToSomeColor
+import com.pv.screendataui.toComposeColor
+import com.pv.screendataui.toSafeComposeColor
 
 @Composable
 fun SDText(someText: SomeText) {
@@ -25,18 +29,21 @@ fun SDText(someText: SomeText) {
 
     Text(
         text = someText.title,
-        modifier = textModifier
+        modifier = textModifier,
+        fontSize = someText.fontSize.sp,
+        color = someText.style.foregroundColor.toSafeComposeColor()
     )
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
-internal fun sdTextPreview() {
+fun sdTextPreview() {
 
     SDText(
         someText = SomeText(
             id = "id",
-            title = "Trying this out",
+            title = "Trying this out tho",
+            fontSize = 14,
             style = SomeStyle(
                 backgroundColor = "#000000".hexToSomeColor(),
                 foregroundColor = "#0080ff".hexToSomeColor(),
