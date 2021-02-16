d325e95a289e8e74b6a818eeb18accf0f6a3fc22
pv <parshav@live.com>
2021-02-15 19:32:11 -0600

    Label functionality updated

--- screendataui/src/main/java/com/pv/screendataui/views/SDLabel.kt
+++ screendataui/src/main/java/com/pv/screendataui/views/SDLabel.kt
@@ -1,12 +1,17 @@
 package com.pv.screendataui.views
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.material.Text
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -17,20 +22,34 @@ import com.pv.screendata.types.Alignment
 import com.pv.screendata.types.FontType
 import com.pv.screendata.views.SomeLabel
 import com.pv.screendataui.toComposeColor
+import com.pv.sddestination.SDDestinationStore
 
 @Composable
 fun SDLabel(label: SomeLabel) {
 
     val padding = label.style?.padding?.dp ?: 0.dp
 
-    val labelModifier = Modifier.fillMaxWidth().then(
-        Modifier.padding(
-            start = padding,
-            end = padding
+    val labelModifier = Modifier.fillMaxWidth()
+        .then(
+            Modifier.clickable(onClick = {
+                SDDestinationStore.desinationHandler?.handeDestination(label.destination)
+            })
         )
-    )
+        .then(
+            Modifier.background(
+                color = label.style?.backgroundColor?.toComposeColor() ?: Color.Transparent,
+                shape = RoundedCornerShape(
+                    size = label.style?.cornerRadius?.dp ?: 0.dp
+                )
+            )
+        )
+        .then(
+            Modifier.padding(
+                all = padding
+            )
+        )
 
-    val textColor = label.style?.foregroundColor?.toComposeColor() ?: Color.Magenta
+    val textColor = label.style?.foregroundColor?.toComposeColor() ?: Color.White
 
     val size = when (label.font) {
         FontType.largeTitle -> 24.sp
@@ -41,7 +60,14 @@ fun SDLabel(label: SomeLabel) {
         FontType.caption -> 12.sp
     }
 
-    Column(modifier = labelModifier) {
+    val horizontalAlignment =
+        if (label.destination != null) androidx.compose.ui.Alignment.CenterHorizontally
+        else androidx.compose.ui.Alignment.Start
+
+    Column(
+        modifier = labelModifier,
+        horizontalAlignment = horizontalAlignment
+    ) {
 
         label.title.takeIf { it.isNotEmpty() }?.let {
             Text(
@@ -64,7 +90,7 @@ fun SDLabel(label: SomeLabel) {
 
 @Preview
 @Composable
-internal fun SDLabelPreview() {
+private fun SDLabelPreview() {
     SDLabel(label = SDLabelMock.mock)
 }
 
@@ -78,6 +104,11 @@ internal object SDLabelMock {
             foregroundColor = SomeColor(
                 1f, 1f, 1f, 1f
             ),
+            backgroundColor = SomeColor(
+                0.5f, 0.5f, 0.5f, 1f
+            ),
+            cornerRadius = 8,
+            padding = 12,
             alignment = Alignment.center
         ),
         destination = null,
